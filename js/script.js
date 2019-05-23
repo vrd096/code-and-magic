a6e51cd556742cc56f32a3031c5cbe5e286be079
Barber <48715486+vrd096@users.noreply.github.com>
2019-05-23 17:00:06 +0500

    добавил скрипт

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var fireballSize = 22;
+var getFireballSpeed = function(left) {
+    if (left === true) {
+        return 5;
+    }
+    return 2;
+}
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getWizardHeight = function() {
+    return 1.337 * wizardWidth;
+}
+var getWizardX = function(width) {
+    
+}
